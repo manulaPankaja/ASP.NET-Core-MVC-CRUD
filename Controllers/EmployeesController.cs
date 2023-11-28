b0dca28c9e73f7217f586e4b9f81859084ce5cb3
manulaPankaja <pankajabad1998@gmail.com>
2023-11-29 00:02:41 +0530

    install EF Core and add new controller

---
+++ Controllers/EmployeesController.cs
@@ -0,0 +1,41 @@
+﻿using ASPNETMVCCRUD.Data;
+using ASPNETMVCCRUD.Models;
+using ASPNETMVCCRUD.Models.Domain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNETMVCCRUD.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly MVCDemoDbContext mvcDemoDbContext;
+
+        public EmployeesController(MVCDemoDbContext mvcDemoDbContext)
+        {
+            this.mvcDemoDbContext = mvcDemoDbContext;
+        }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddEmployeeViewModel addEmployeeRequest)
+        {
+            var employee = new Employee()
+            {
+                Id = Guid.NewGuid(),
+                Name = addEmployeeRequest.Name,
+                Emil = addEmployeeRequest.Email,
+                Salary = addEmployeeRequest.Salary,
+                Department = addEmployeeRequest.Department,
+                DateOfBirth = addEmployeeRequest.DateOfBirth,
+            };
+
+            await mvcDemoDbContext.Employees.AddAsync(employee);
+            await mvcDemoDbContext.SaveChangesAsync();
+
+            return RedirectToAction("Add");
+        }
+    }
+}
---
+++ Models/Domain/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace ASPNETMVCCRUD.Models.Domain
+{
+    public class Employee
+    {
+        public Guid Id {get; set;}
+        public string Name { get; set;}
+        public string Emil { get; set; }
+        public int Salary { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string Department { get; set; }
+    }
+}
